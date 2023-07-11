fb4ad8138554abdb13facca8d7349ca4aec69495
KaileyWoo <shouyingwu@foxmail.com>
2023-07-11 15:06:20 +0800

    modify reward file

--- env/obsToState.py
+++ env/obsToState.py
@@ -15,6 +15,8 @@ class ObsToState:
         self.WEZ_Min = 200
         self.WEZ_Max = 1000
         self.Distance_Max = 10000
+        self.ALT_Min = 1000
+        self.ALT_Max = 10000
         self.TotalReward = 0
 
     # 获得状态奖励结束标志
@@ -43,28 +45,121 @@ class ObsToState:
         next_sparse_reward, next_potential_reward = self.getReward(next_self_track, next_DetectedInfo, False)
 
         # reward function
-        self.CurTotalReward = cur_sparse_reward + 0.99*next_potential_reward - self.CurPotentialReward # + 0.001*math.tanh(-self.CurTime / 300)
+        self.CurTotalReward = cur_sparse_reward + next_potential_reward - self.CurPotentialReward # + 0.001*math.tanh(-self.CurTime / 300)
         # termination probability
         terminate_prob = self.CurPotentialReward
         if terminate_prob < 0:
             terminate_prob = 0
 
-        self.TotalReward = self.TotalReward + self.CurTotalReward
+        self.is_termination(terminate_prob, cur_self_track)
 
+        return STATE, self.CurTotalReward, self.isDone
+
+    def is_termination(self, terminate_prob, cur_self_info):
+        #判断是否结束
         if terminate_prob > 0.7:
             self.isDone = 1
-            print("----------------------------------------------目标达成！-------------------------------------------------")
-            print("本局时间: ", self.CurTime, ", Cur_potentialR=", self.CurPotentialReward, ", Next_potentialR=", next_potential_reward,
-                  ", TotalReward=", self.TotalReward)
-            print(", 高度:", cur_self_track['Altitude'], ", distance=", self.CurDistance,
+            terminal_reward = 50 + math.exp(-2*self.CurTime/300)
+            self.CurTotalReward += terminal_reward
+            self.TotalReward += self.CurTotalReward
+            print("--------------------------------------------------------目标达成！---------------------------------------------------------------------")
+            print("本局时间: ", self.CurTime, ", TotalReward=", self.TotalReward)
+            print(", 高度:", cur_self_info['Altitude'], ", distance=", self.CurDistance,
                   ", attackAngle=", self.CurAttackAngle*180/math.pi, ", escapeAngle=", self.CurEscapeAngle*180/math.pi)
-        else:
-            self.isDone = self.is_termination(cur_self_track)
+
+        elif self.CurDistance > self.Distance_Max:
+            self.isDone = 2
+            terminal_reward = -50 + math.tanh(-math.fabs(self.CurAttackAngle) / math.pi)
+            self.CurTotalReward += terminal_reward
+            self.TotalReward += self.CurTotalReward
+            print("超出最大距离范围，结束  时间: ", self.CurTime, ", TotalReward=", self.TotalReward, ", CurAttackAngle=",
+                  self.CurAttackAngle * 180 / math.pi)
+
+        elif cur_self_info['Altitude'] > self.ALT_Max or cur_self_info['Altitude'] < self.ALT_Min:
+            self.isDone = 2
+            terminal_reward = -40 + math.tanh((self.WEZ_Min - self.CurDistance) / self.Distance_Max)
+            self.CurTotalReward += terminal_reward
+            self.TotalReward += self.CurTotalReward
+            print("高度越界，结束  时间: ", self.CurTime, ", TotalReward=", self.TotalReward, ", distance=", self.CurDistance)
+
+        elif self.CurTime > 299:
+            self.isDone = 3
+            terminal_reward = -30 + math.tanh((self.WEZ_Min - self.CurDistance) / self.Distance_Max)
+            self.CurTotalReward += terminal_reward
+            self.TotalReward += self.CurTotalReward
+            print("---超时---，", "TotalReward=", self.TotalReward, ", distance=", self.CurDistance)
 
         if self.isDone != 0:
             self.TotalReward = 0
 
-        return STATE, self.CurTotalReward, self.isDone
+    # 计算奖励
+    def getReward(self, self_info, dete_info, curFlag=False):
+        # 计算两机距离
+        distance = RongAoUtils.getDistance3D(dete_info)
+        # 计算攻击角[-pi, pi]、逃逸角[-pi, pi]
+        distanceVector3D = RongAoUtils.getDistanceVector3D(dete_info)
+        selfPlaneSpeedVector = RongAoUtils.getSpeedVector3D(self_info)
+        detePlaneSpeedVector = RongAoUtils.getSpeedVector3D(dete_info)
+        attackAngle = math.acos(
+            (selfPlaneSpeedVector[0] * distanceVector3D[0] + selfPlaneSpeedVector[1] * distanceVector3D[1] +
+             selfPlaneSpeedVector[2] * distanceVector3D[2]) / distance)
+        escapeAngle = math.acos(
+            (detePlaneSpeedVector[0] * distanceVector3D[0] + detePlaneSpeedVector[1] * distanceVector3D[1] +
+             detePlaneSpeedVector[2] * distanceVector3D[2]) / distance)
+
+        # sparse reward function
+        sparse_reward = 0
+        # 敌机在我机范围内
+        if math.fabs(attackAngle) <= math.pi / 3 and math.fabs(escapeAngle) <= math.pi / 3 and \
+                (distance >= self.WEZ_Min and distance <= self.WEZ_Max):
+            sparse_reward = 1
+        # 我机在敌机范围内
+        if math.fabs(attackAngle) >= 2 * math.pi / 3 and math.fabs(escapeAngle) >= 2 * math.pi / 3 and \
+                (distance >= self.WEZ_Min and distance <= self.WEZ_Max):
+            sparse_reward = -1
+
+        # potential function
+        potential_reward = (1 - (math.fabs(attackAngle) / math.pi + math.fabs(escapeAngle) / math.pi)) * \
+                           math.exp(-0.0002 * math.fabs(distance - self.WEZ_Min))
+
+        if curFlag is True:
+            self.CurDistance = distance
+            self.CurAttackAngle = attackAngle
+            self.CurEscapeAngle = escapeAngle
+
+        return sparse_reward, potential_reward
+
+
+    # 获得一帧的状态
+    def getOneFrameState(self, self_track, detectedInfo):
+        # 提取1帧观测数据
+        # 经纬度差[-180,180]、[-90,90]
+        lon_diff = (detectedInfo['Longitude'] - self_track['Longitude']) / 180.0
+        lat_diff = (detectedInfo['Latitude'] - self_track['Latitude']) / 90.0
+        # 高度
+        self_alt = self_track['Altitude'] / self.ALT_Max
+        dete_alt = detectedInfo['Altitude'] / self.ALT_Max
+        # 速度分量
+        self_v_n = self_track['V_N'] / 600.0
+        self_v_e = self_track['V_E'] / 600.0
+        self_v_d = self_track['V_D'] / 600.0
+        # 速度差[0, 600]
+        self_v_real = RongAoUtils.getSpeed(self_track)
+        dete_v_real = RongAoUtils.getSpeed(detectedInfo)
+        v_diff = (dete_v_real - self_v_real) / 600.0
+        # 偏航[0, 2*pi]、俯仰[-pi/2, pi/2]、滚转[-pi, pi]
+        self_heading = self_track['Heading'] / (2 * math.pi)
+        self_pitch = self_track['Pitch'] / (math.pi / 2)
+        self_roll = self_track['Roll'] / math.pi
+        dete_heading = detectedInfo['Heading'] / (2 * math.pi)
+        dete_pitch = detectedInfo['Pitch'] / (math.pi / 2)
+        dete_roll = detectedInfo['Roll'] / math.pi
+        # 状态向量
+        one_state = np.array([lon_diff, lat_diff, self_alt, dete_alt, self_v_n, self_v_e, self_v_d, v_diff,
+                              self_heading, self_pitch, self_roll, dete_heading, dete_pitch, dete_roll],
+                             dtype=np.float32)
+
+        return one_state
 
     # 解析一帧数据
     def parseOneFrameData(self, observation):
@@ -93,85 +188,8 @@ class ObsToState:
 
         return cur_self_track, cur_DetectedInfo
 
-    # 获得一帧的状态
-    def getOneFrameState(self, self_track, detectedInfo):
-        # 提取1帧观测数据
-        # 经纬度差[-180,180]、[-90,90]
-        lon_diff = (detectedInfo['Longitude'] - self_track['Longitude']) / 180.0
-        lat_diff = (detectedInfo['Latitude'] - self_track['Latitude']) / 90.0
-        # 高度[1000, 10000]
-        self_alt = self_track['Altitude'] / 10000.0
-        dete_alt = detectedInfo['Altitude'] / 10000.0
-        # 速度分量[0, 600]
-        self_v_n = self_track['V_N'] / 600.0
-        self_v_e = self_track['V_E'] / 600.0
-        self_v_d = self_track['V_D'] / 600.0
-        # 速度差[0, 600]
-        self_v_real = RongAoUtils.getSpeed(self_track)
-        dete_v_real = RongAoUtils.getSpeed(detectedInfo)
-        v_diff = (dete_v_real - self_v_real) / 600.0
-        # 偏航[0, 2*pi]、俯仰[-pi/2, pi/2]、滚转[-pi, pi]
-        self_heading = self_track['Heading'] / (2*math.pi)
-        self_pitch = self_track['Pitch'] / (math.pi/2)
-        self_roll = self_track['Roll'] / math.pi
-        dete_heading = detectedInfo['Heading'] / (2*math.pi)
-        dete_pitch = detectedInfo['Pitch'] / (math.pi/2)
-        dete_roll = detectedInfo['Roll'] / math.pi
-        # 状态向量
-        one_state = np.array([lon_diff, lat_diff, self_alt, dete_alt, self_v_n, self_v_e, self_v_d, v_diff,
-                              self_heading, self_pitch, self_roll, dete_heading, dete_pitch, dete_roll], dtype=np.float32)
 
-        return one_state
 
-    # 计算奖励
-    def getReward(self, self_info, dete_info, curFlag=False):
-        #计算两机距离
-        distance = RongAoUtils.getDistance3D(dete_info)
-        #计算攻击角[-pi, pi]、逃逸角[-pi, pi]
-        distanceVector3D = RongAoUtils.getDistanceVector3D(dete_info)
-        selfPlaneSpeedVector = RongAoUtils.getSpeedVector3D(self_info)
-        detePlaneSpeedVector = RongAoUtils.getSpeedVector3D(dete_info)
-        attackAngle = math.acos((selfPlaneSpeedVector[0] * distanceVector3D[0] + selfPlaneSpeedVector[1] * distanceVector3D[1] +
-                                 selfPlaneSpeedVector[2] * distanceVector3D[2]) / distance)
-        escapeAngle = math.acos((detePlaneSpeedVector[0] * distanceVector3D[0] + detePlaneSpeedVector[1] * distanceVector3D[1] +
-                                 detePlaneSpeedVector[2] * distanceVector3D[2]) / distance)
 
-        #sparse reward function
-        sparse_reward = 0
-        #敌机在我机范围内
-        if math.fabs(attackAngle) <= math.pi/3 and math.fabs(escapeAngle) <= math.pi/3 and \
-                (distance >= self.WEZ_Min and distance <= self.WEZ_Max):
-            sparse_reward = 1
-        #我机在敌机范围内
-        if math.fabs(attackAngle) >= 2*math.pi/3 and math.fabs(escapeAngle) >= 2*math.pi/3 and \
-                (distance >= self.WEZ_Min and distance <= self.WEZ_Max):
-            sparse_reward = -1
-
-        #potential function
-        potential_reward = (1-(math.fabs(attackAngle)/math.pi+math.fabs(escapeAngle)/math.pi))*\
-                           math.exp(-0.0003*math.fabs(distance-self.WEZ_Min))
-
-        if curFlag is True:
-            self.CurDistance = distance
-            self.CurAttackAngle = attackAngle
-            self.CurEscapeAngle = escapeAngle
-
-        return sparse_reward, potential_reward
-
-    def is_termination(self, cur_self_info):
-        #判断是否结束
-        Done = 0
-        if cur_self_info['Altitude'] > 10000 or cur_self_info['Altitude'] < 1000:
-            Done = 2
-            print("高度越界，结束  时间: ", self.CurTime, ", Cur_potentialR=", self.CurPotentialReward,
-                  ", TotalReward=", self.TotalReward, ", distance=", self.CurDistance)
-        elif self.CurDistance > self.Distance_Max:
-            Done = 2
-            print("超出最大距离范围，结束  时间: ", self.CurTime, ", Cur_potentialR=", self.CurPotentialReward, ", TotalReward=", self.TotalReward)
-        elif self.CurTime > 299:
-            Done = 3
-            print("---超时---，", ", Cur_potentialR=", self.CurPotentialReward, "TotalReward=", self.TotalReward, ", distance=", self.CurDistance)
-
-        return Done
 
 
--- models/params.py
+++ models/params.py
@@ -12,23 +12,23 @@ max_episodes=int(1e5)     # (int) – Maximum number of episodes to run
 
 StateDim = (42, )  # 状态维度
 ActionDim = (4, )   # 动作维度
-Train = 0      # 0，1，2分别表示：0重新训练，1加载之前的训练，2测试模式
+Train = 1      # 0，1，2分别表示：0重新训练，1加载之前的训练，2测试模式
 
 
 class FolderPath:
     def __init__(self):
         # Red
-        save_date = '2023_07_10'
-        load_date = '2023_07_06'
-        model_name = "sac_model_3600000_steps.zip"
+        save_date = '2023_07_11'
+        load_date = '2023_07_10'
+        model_name = "sac_model_2000000_steps.zip"
         self.Save_ModelDir_Red = "./models/Red/"+save_date+"/sac_checkpoints/"
         self.Load_ModelDir_Red = "./models/Red/"+load_date+"/sac_checkpoints/"+model_name
         # self.LogDir_Red = "./logs/Red/"+save_date+"/sac_tensorboard/"
         self.LogDir_Red = "./logs/Red/"+save_date+"/"
         # Blue
-        self.Save_ModelDir_Blue = "./models/Blue/"+save_date+"/sac_checkpoints/"
-        self.Load_ModelDir_Blue = "./models/Blue/"+load_date+"/sac_checkpoints/"+model_name
-        self.LogDir_Blue = "./logs/Blue/"+save_date+"/"
+        # self.Save_ModelDir_Blue = "./models/Blue/"+save_date+"/sac_checkpoints/"
+        # self.Load_ModelDir_Blue = "./models/Blue/"+load_date+"/sac_checkpoints/"+model_name
+        # self.LogDir_Blue = "./logs/Blue/"+save_date+"/"
 
         self.create_folders()
 
@@ -36,7 +36,7 @@ class FolderPath:
         if not os.path.exists(self.Save_ModelDir_Red):
             os.makedirs(self.Save_ModelDir_Red)
         if not os.path.exists(self.Load_ModelDir_Red):
-            os.makedirs(self.Load_ModelDir_Red)
+            print("加载模型路径错误！ 路径：" + self.Load_ModelDir_Red)
         if not os.path.exists(self.LogDir_Red):
             os.makedirs(self.LogDir_Red)
 
