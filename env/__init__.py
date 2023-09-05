58dfe6ccde7233f42a46e511968fe0314336e2c0
KaileyWoo <shouyingwu531@gmail.com>
2023-09-05 17:38:48 +0800

    modify

--- env/__init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.envs.registration import register
-
-register(
-    id='MyCombatEnv-v0',
-    entry_point='env.MyCombatEnv:MyCombatEnv',
-    # Max number of steps per episode, using a `TimeLimitWrapper`
-    #max_episode_steps=500,
-)
